8b14fb30f74027e4a9815f68c12b7bc31bd69864
Suhendra Utet <suhendra.utet@supplylogix.io>
2020-03-14 20:20:01 -0500

    add table storage input binding

--- send-secret/__init__.py
+++ send-secret/__init__.py
@@ -1,9 +1,7 @@
 import logging
-
 import azure.functions as func
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, message: func.Out[str]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
