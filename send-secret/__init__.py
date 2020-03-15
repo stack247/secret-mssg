74ba796bf4b8c93d40c5ce40de448afb8bb003d1
stack247 <suhendra.hui@outlook.com>
2020-03-14 23:02:53 -0500

    get posted message, lots of todo.

--- send-secret/__init__.py
+++ send-secret/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import requests
+import json
 import azure.functions as func
 
 
@@ -9,20 +10,30 @@ def main(req: func.HttpRequest, message: func.Out[str]) -> func.HttpResponse:
 
     bot_token = os.environ['BotToken']
 
-    post(bot_token, '', '')
+    # Get posted message.
+    posted_message = req.get_body().decode('utf-8')
+    print('-=====- posted_message', posted_message)
+    
+    # TODO: Parse posted message
 
-    data = {
-      'PartitionKey': '',
-      'RowKey': '',
-      'channel': '',
-      'ts': ''
-    }
-    message.set(json.dumps(data))
+    # Post message
+    res = post(bot_token, '', 'test message')
+
+    # TODO: parse result
+
+    # TODO: Store secret to delete
+    #data = {
+    #  'PartitionKey': '',
+    #  'RowKey': '',
+    #  'channel': '',
+    #  'ts': ''
+    #}
+    #message.set(json.dumps(data))
     
     return func.HttpResponse(status_code=200)
 
 # TODO: move to common class.
-def post(token: str, channel: str, message: str):
+def post(token: str, channel: str, message: str) -> str:
     url = f'https://slack.com/api/chat.postMessage?token={token}&channel={channel}&text={message}'
     res = requests.get(url=url)
-    
+    return res.text
